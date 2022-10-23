637dbfdf556b5c063c63e5aa50c82d67cee889b0
mrchdm <dementevamm@yandex.ru>
2022-10-23 22:39:46 +0300

    дз пятого семинара

---
+++ hw5.py
@@ -0,0 +1,40 @@
+# Напишите программу, удаляющую из текста все слова, содержащие абв
+# text = input()
+# text = text.split()
+# new_text =''
+# for i in text:
+#     if 'abc' not in i:
+#         new_text += i + ' '
+# print(new_text)
+
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+
+s = "aaaavvvvrrrrryyy"
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
